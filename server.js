359dea6bff631c024ffda13aebb2f1380076434d
Hack Reactor Student <student@hackreactor.com>
2017-09-18 15:22:11 -0400

    v2

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const program = require('commander');
 const { prompt } = require('inquirer');
 
-const board = [
-[' ', ' ', ' '],
-[' ', ' ', ' '],
-[' ', ' ', ' '],
+const newBoard = [
+['0', '1', '2'],
+['3', '4', '5'],
+['6', '7', '8'],
 ];
+let board = newBoard.map(row => row.slice());
 let turnCount = 0;
 
 program
@@ -33,21 +34,13 @@ const turn = () => {
       name: 'move',
       message: (turnCount % 2 === 0 ? 'X' : 'O') + ': what space do you choose? (0-8)'
     }]).then(({move}) => {
-      //console.log(Math.floor(move / 3), Math.floor(move % 3 + 2))
-      // if (move === 'board') {
-      //   console.log(
-      //     `  
-      //      0 | 1 | 2
-      //      --|---|--
-      //      3 | 4 | 5
-      //      --|---|--
-      //      6 | 7 | 8  
-      //     `   
-      //   );
-      //   turn();
-      // }
       let row = Math.floor(move / 3);
       let col = move % 3;
+      if (board[row][col] === 'X' || board[row][col] === 'O') {
+        console.log('That space is already taken.');
+        turn();
+        return;
+      }
       board[row][col] = turnCount % 2 === 0 ? 'X' : 'O';
       console.log(
         `  
@@ -63,13 +56,40 @@ const turn = () => {
 };
 
 const checkWin = () => {
-  const win = false;
-  // if (board[0][0] === board[0][1] && board[0][0] === board[0][2] ||
-  //   board[1][0] === board[1][1] && 
-  //   board[0][0] === board[1][0] && board[0][0] === board[0][2] ||
-  //   board[0][1] === board[1][1] && board[2][1] === board[0][1] ||)
-  if (false) {
+  const win =
+    // rows
+    (board[0][0] === board[0][1] && board[0][0] === board[0][2]) ||
+    (board[1][0] === board[1][1] && board[1][0] === board[1][2]) ||
+    (board[2][0] === board[2][1] && board[2][0] === board[2][2]) ||
+    // cols
+    (board[0][0] === board[1][0] && board[0][0] === board[2][0]) ||
+    (board[0][1] === board[1][1] && board[0][1] === board[2][1]) ||
+    (board[0][2] === board[1][2] && board[0][1] === board[2][2]) ||
+    // diag
+    (board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+    (board[0][2] === board[1][1] && board[0][2] === board[2][0]);
+  if (win) {
     console.log('!!! ' + (turnCount % 2 === 0 ? 'X' : 'O') + ' WON !!!');
+    prompt([{
+      type: 'confirm',
+      name: 'play',
+      message: 'play again?',
+    }]).then(({play}) => {
+      if (play) {
+        board = newBoard.map(row => row.slice());
+        console.log(
+          `  
+           ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
+           --|---|--
+           ${board[1][0]} | ${board[1][1]} | ${board[1][2]}
+           --|---|--
+           ${board[2][0]} | ${board[2][1]} | ${board[2][2]}  
+          `   
+        );
+        turnCount++;
+        turn();
+      }
+    });
   } else{
     turnCount++;
     turn();
